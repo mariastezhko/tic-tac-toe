e22b14a5e79857a69a88a8da335319b6014b3d7e
Maria Stezhko <stezhko@gmail.com>
2018-03-08 11:50:01 -0800

    feat: Add functionality for clicking the buttons

--- TTT/ViewController.swift
+++ TTT/ViewController.swift
@@ -9,7 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var clickCount = 0
+    var currentColor: String = "grey"
+    
+    @IBAction func btn8(_ sender: UIButton) {
+        btnClick(button: sender)
+    }
+    @IBAction func btn9(_ sender: UIButton) {
+        btnClick(button: sender)
+    }
+    @IBAction func btn6(_ sender: UIButton) {
+        btnClick(button: sender)
+    }
+    @IBAction func btn7(_ sender: UIButton) {
+        btnClick(button: sender)
+    }
+    @IBAction func btn5(_ sender: UIButton) {
+        btnClick(button: sender)
+    }
+    @IBAction func btn4(_ sender: UIButton) {
+        btnClick(button: sender)
+    }
+    @IBAction func btn3(_ sender: UIButton) {
+        btnClick(button: sender)
+    }
+    @IBAction func btn2(_ sender: UIButton) {
+        btnClick(button: sender)
+    }
+    @IBAction func btn1(_ sender: UIButton) {
+        btnClick(button: sender)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +50,29 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    func btnClick(button: UIButton) {
+        if clickCount == 0 {
+            button.backgroundColor = UIColor.red
+            clickCount += 1
+            currentColor = "red"
+            button.isEnabled = false
+        }
+        else {
+            clickCount += 1
+            if currentColor == "red" {
+                    button.backgroundColor = UIColor.blue
+                    currentColor = "blue"
+                    button.isEnabled = false
+                }
+                else {
+                    button.backgroundColor = UIColor.red
+                    currentColor = "red"
+                    button.isEnabled = false
+                }
+            }
+        }
+        
+    
 
 }
 

24f78b7dff4b606e2f21bd6716a2574a03617558
Maria Stezhko <stezhko@gmail.com>
2018-03-09 15:27:29 -0800

    feat: Add game functionality

--- TTT/ViewController.swift
+++ TTT/ViewController.swift
@@ -10,36 +10,98 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var winnerLabel: UILabel!
     var clickCount = 0
-    var currentColor: String = "grey"
+    var currentColor: String = "gray"
     
-    @IBAction func btn8(_ sender: UIButton) {
-        btnClick(button: sender)
+    @IBOutlet weak var btn9: UIButton!
+    @IBOutlet weak var btn8: UIButton!
+    @IBOutlet weak var btn7: UIButton!
+    @IBOutlet weak var btn6: UIButton!
+    @IBOutlet weak var btn5: UIButton!
+    @IBOutlet weak var btn4: UIButton!
+    @IBOutlet weak var btn3: UIButton!
+    @IBOutlet weak var btn2: UIButton!
+    @IBOutlet weak var btn1: UIButton!
+    
+    @IBOutlet var buttons: [UIButton]!
+    
+    // Needs refactoring
+    @IBAction func btnAction(_ sender: UIButton) {
+        switch sender {
+            case btn1:
+                btnClick(button: btn1)
+                if ((btn2.backgroundColor == btn1.backgroundColor) && (btn3.backgroundColor == btn1.backgroundColor)) || ((btn4.backgroundColor == btn1.backgroundColor) && (btn7.backgroundColor == btn1.backgroundColor)) || ((btn5.backgroundColor == btn1.backgroundColor) && (btn9.backgroundColor == btn1.backgroundColor)) {
+                    print("Congrats \(currentColor) won!")
+                    winnerLabel.text = "Congrats \(currentColor) won!"
+                    buttonsDisable()
+                }
+            case btn2:
+                btnClick(button: btn2)
+                if ((btn1.backgroundColor == btn2.backgroundColor) && (btn3.backgroundColor == btn2.backgroundColor)) || ((btn5.backgroundColor == btn2.backgroundColor) && (btn8.backgroundColor == btn2.backgroundColor)) {
+                    print("Congrats \(currentColor) won!")
+                    winnerLabel.text = "Congrats \(currentColor) won!"
+                    buttonsDisable()
+                }
+            case btn3:
+                btnClick(button: btn3)
+                if ((btn2.backgroundColor == btn3.backgroundColor) && (btn1.backgroundColor == btn3.backgroundColor)) || ((btn4.backgroundColor == btn3.backgroundColor) && (btn7.backgroundColor == btn3.backgroundColor)) || ((btn5.backgroundColor == btn3.backgroundColor) && (btn9.backgroundColor == btn3.backgroundColor)) {
+                    print("Congrats \(currentColor) won!")
+                    winnerLabel.text = "Congrats \(currentColor) won!"
+                    buttonsDisable()
+                }
+            case btn4:
+                btnClick(button: btn4)
+                if ((btn1.backgroundColor == btn4.backgroundColor) && (btn7.backgroundColor == btn4.backgroundColor)) || ((btn5.backgroundColor == btn4.backgroundColor) && (btn6.backgroundColor == btn4.backgroundColor)) {
+                    print("Congrats \(currentColor) won!")
+                    winnerLabel.text = "Congrats \(currentColor) won!"
+                    buttonsDisable()
+            }
+            case btn5:
+                btnClick(button: btn5)
+                if ((btn2.backgroundColor == btn5.backgroundColor) && (btn8.backgroundColor == btn5.backgroundColor)) || ((btn4.backgroundColor == btn5.backgroundColor) && (btn6.backgroundColor == btn5.backgroundColor)) || ((btn1.backgroundColor == btn5.backgroundColor) && (btn9.backgroundColor == btn5.backgroundColor)) {
+                    print("Congrats \(currentColor) won!")
+                    winnerLabel.text = "Congrats \(currentColor) won!"
+                    buttonsDisable()
+            }
+            case btn6:
+                btnClick(button: btn6)
+                if ((btn4.backgroundColor == btn6.backgroundColor) && (btn5.backgroundColor == btn6.backgroundColor)) || ((btn3.backgroundColor == btn6.backgroundColor) && (btn9.backgroundColor == btn6.backgroundColor)) {
+                    print("Congrats \(currentColor) won!")
+                    winnerLabel.text = "Congrats \(currentColor) won!"
+                    buttonsDisable()
+            }
+            case btn7:
+                btnClick(button: btn7)
+                if ((btn1.backgroundColor == btn7.backgroundColor) && (btn4.backgroundColor == btn7.backgroundColor)) || ((btn8.backgroundColor == btn7.backgroundColor) && (btn9.backgroundColor == btn7.backgroundColor)) || ((btn5.backgroundColor == btn7.backgroundColor) && (btn3.backgroundColor == btn7.backgroundColor)) {
+                    print("Congrats \(currentColor) won!")
+                    winnerLabel.text = "Congrats \(currentColor) won!"
+                    buttonsDisable()
+            }
+            case btn8:
+                btnClick(button: btn8)
+                if ((btn2.backgroundColor == btn2.backgroundColor) && (btn5.backgroundColor == btn2.backgroundColor)) || ((btn7.backgroundColor == btn2.backgroundColor) && (btn9.backgroundColor == btn2.backgroundColor)) {
+                    print("Congrats \(currentColor) won!")
+                    winnerLabel.text = "Congrats \(currentColor) won!"
+                    buttonsDisable()
+            }
+            case btn9:
+                btnClick(button: btn9)
+                if ((btn6.backgroundColor == btn9.backgroundColor) && (btn3.backgroundColor == btn9.backgroundColor)) || ((btn8.backgroundColor == btn9.backgroundColor) && (btn7.backgroundColor == btn9.backgroundColor)) || ((btn5.backgroundColor == btn9.backgroundColor) && (btn1.backgroundColor == btn9.backgroundColor)) {
+                    print("Congrats \(currentColor) won!")
+                    winnerLabel.text = "Congrats \(currentColor) won!"
+                    buttonsDisable()
+            }
+            default:
+                break
+        }
     }
-    @IBAction func btn9(_ sender: UIButton) {
-        btnClick(button: sender)
+    
+    @IBAction func resetClicked(_ sender: UIButton) {
+        reset()
     }
-    @IBAction func btn6(_ sender: UIButton) {
-        btnClick(button: sender)
-    }
-    @IBAction func btn7(_ sender: UIButton) {
-        btnClick(button: sender)
-    }
-    @IBAction func btn5(_ sender: UIButton) {
-        btnClick(button: sender)
-    }
-    @IBAction func btn4(_ sender: UIButton) {
-        btnClick(button: sender)
-    }
-    @IBAction func btn3(_ sender: UIButton) {
-        btnClick(button: sender)
-    }
-    @IBAction func btn2(_ sender: UIButton) {
-        btnClick(button: sender)
-    }
-    @IBAction func btn1(_ sender: UIButton) {
-        btnClick(button: sender)
-    }
+    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -72,7 +134,19 @@ class ViewController: UIViewController {
             }
         }
         
-    
+    func reset() {
+        clickCount = 0
+        currentColor = "gray"
+        for i in 0..<buttons.count {
+            buttons[i].backgroundColor = UIColor.lightGray
+            buttons[i].isEnabled = true
+        }
+    }
+    func buttonsDisable() {
+        for i in 0..<buttons.count {
+            buttons[i].isEnabled = false
+        }
+    }
 
 }
 
